c087829a53a5f333adff1c6a76d6705225f50e28
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-07-09 02:46:54 +0530

    main Screen (WIP)

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -28,13 +30,15 @@ class MainScreen extends StatelessWidget {
                       (
                         width: 50,
                         height: 50,
-                        decoration: const BoxDecoration
+                        
+                        decoration:  BoxDecoration
                         (
                           shape: BoxShape.circle,
-                          color: Colors.black,
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black),
                         ),
                       ),
-                      const Icon(Icons.person , color: Colors.white,)
+                      const Icon(Icons.person , color: Colors.black,)
                       ]  
                     
                     ),
@@ -76,11 +80,22 @@ class MainScreen extends StatelessWidget {
             (
               width:  MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.width /2,
-              decoration: const BoxDecoration
+              decoration:  BoxDecoration
               (
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(20))
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+                border: Border.all(color: Colors.black),
+                boxShadow: 
+                const [
+                  BoxShadow
+                  (
+                    blurRadius: 10,
+                    color: Colors.grey,
+                    offset: Offset(0, 5)
+                  )
+                ] 
               ),
+              
               child:  const Column
               (
                 
@@ -92,7 +107,7 @@ class MainScreen extends StatelessWidget {
                     "TOTAL BALANCE",
                     style: TextStyle
                     (
-                      color: Colors.white,
+                      color: Colors.grey,
                       fontSize: 20,
                       letterSpacing: 4
                     ),
@@ -103,7 +118,7 @@ class MainScreen extends StatelessWidget {
                     "\$ 20,000",
                     style: TextStyle
                     (
-                      color: Colors.white,
+                      
                       fontSize: 40,
                       fontWeight: FontWeight.bold
                     ),
@@ -138,7 +153,7 @@ class MainScreen extends StatelessWidget {
                                 "Income",
                                 style: TextStyle
                                 (
-                                  color: Colors.white,
+                                  color: Colors.black,
                                   fontSize: 18
                                 ),
                           
@@ -146,10 +161,10 @@ class MainScreen extends StatelessWidget {
                               
                               Text
                               (
-                                "\$ 2000",
+                                "\$ 2500",
                                 style: TextStyle
                                 (
-                                  color: Colors.white,
+                                  color: Colors.black,
                                   fontSize: 20
                                 ),
                           
@@ -178,10 +193,10 @@ class MainScreen extends StatelessWidget {
                             [
                               Text
                               (
-                                "Income",
+                                "Expense",
                                 style: TextStyle
                                 (
-                                  color: Colors.white,
+                                  color: Colors.black,
                                   fontSize: 18
                                 ),
                           
@@ -191,7 +206,7 @@ class MainScreen extends StatelessWidget {
                                 "\$ 2000",
                                 style: TextStyle
                                 (
-                                  color: Colors.white,
+                                  color: Colors.black,
                                   fontSize: 20
                                 ),
                           
@@ -201,16 +216,153 @@ class MainScreen extends StatelessWidget {
                         ],
                       )
                     ],
+                    
                   )
+                  
+                  
                 ]
                 
               ),
               
+              
+            ),
+            const SizedBox(height: 20,),
+
+             Row
+            (
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: 
+              [
+                const Text
+                (
+                  "TRANSECTION",
+                  style: TextStyle
+                  (
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text
+                  (
+                    "View All",
+                    style: TextStyle
+                    (
+                      color: Colors.grey,
+                      fontSize: 14,
+                      
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+            const SizedBox(height: 20,),
+
+            Expanded(
+              child: ListView.builder
+              (
+                itemCount: 2,
+                itemBuilder: (context , int i)
+              {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container
+                  (
+                    decoration: BoxDecoration
+                    (
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black),
+                      boxShadow: 
+                      const [
+                      BoxShadow
+                      (
+                        blurRadius: 5,
+                        color: Colors.grey,
+                        offset: Offset(0, 5)
+                        
+                      )
+                      ] 
+                    
+                    ),
+                    child:   const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row
+                        (
+
+                          children: 
+                          [
+                            Padding
+                            (
+                              padding: EdgeInsets.all(10.0),
+                              child:  FaIcon(FontAwesomeIcons.dollarSign),
+                              // child: Icon(Icons.attach_money_rounded),
+                            ),
+                            
+                            Text
+                            (
+                              "FOOD",
+                              style: TextStyle
+                              (
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                        
+                              ),
+                            )
+                        
+                          ],
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Column
+                          (
+                            children: 
+                            [
+                              Text
+                              (
+                                "-\$ 54",
+                                style: TextStyle
+                                (
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                          
+                              Text
+                              (
+                                "Today",
+                                style: TextStyle
+                                (
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w300
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }
+              ),
             )
           ],
+          
+        
         
         ),
+        
+        
       ),
+      
+
     );
   }
 }
