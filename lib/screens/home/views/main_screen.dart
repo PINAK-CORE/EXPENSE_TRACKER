e7c55dbdfd28d2667a3a9712d1f409ec84a0a16b
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-07-09 00:32:02 +0530

    main screen (WIP)

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -12,49 +13,199 @@ class MainScreen extends StatelessWidget {
         (
           children: 
           [
-            Row
-            (
-              children: 
-              [
-                Container
-                (
-                  width: 50,
-                  height: 50,
-                  decoration: const BoxDecoration
-                  (
-                    shape: BoxShape.circle,
-                    color: Colors.yellow,
-                  ),
-                ),
-
-                const SizedBox(width: 20,),
-                 const Column
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Row
                 (
                   children: 
                   [
-                    Text
-                    (
-                      'Welcome !',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14
+                    Stack(
+                      alignment: Alignment.center,
+                      children:
+                      [
+                      Container
+                      (
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration
+                        (
+                          shape: BoxShape.circle,
+                          color: Colors.black,
+                        ),
                       ),
+                      const Icon(Icons.person , color: Colors.white,)
+                      ]  
+                    
                     ),
-
-                    Text
+                
+                    const SizedBox(width: 20,),
+                     const Column
                     (
-                      "PINAK",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 3
-
+                      children: 
+                      [
+                        Text
+                        (
+                          'Welcome !',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14
+                          ),
+                        ),
+                
+                        Text
+                        (
+                          "PINAK",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 5
+                
+                            )
                         )
+                      ],
                     )
                   ],
-                )
+                ),
+                const Icon(Icons.settings)
               ],
+            ),
+            const SizedBox(height: 20,),
+            Container
+            (
+              width:  MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width /2,
+              decoration: const BoxDecoration
+              (
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(20))
+              ),
+              child:  const Column
+              (
+                
+                children: 
+                [
+                  SizedBox(height: 25,),
+                  Text
+                  (
+                    "TOTAL BALANCE",
+                    style: TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 20,
+                      letterSpacing: 4
+                    ),
+                  ),
+                  
+                  Text
+                  (
+                    "\$ 20,000",
+                    style: TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  
+                  Row
+                  (
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: 
+                    [
+                      Row
+                      (
+                        children: 
+                        [
+                          Icon
+                          (
+                            Icons.arrow_downward_outlined , 
+                            color: Colors.green,
+                            size: 45,
+                            
+                            
+                          ),
+                          SizedBox(width: 10,),
+                          Column
+                          (
+                            children: 
+                            [
+                              
+                              Text
+                              (
+                                "Income",
+                                style: TextStyle
+                                (
+                                  color: Colors.white,
+                                  fontSize: 18
+                                ),
+                          
+                              )    ,
+                              
+                              Text
+                              (
+                                "\$ 2000",
+                                style: TextStyle
+                                (
+                                  color: Colors.white,
+                                  fontSize: 20
+                                ),
+                          
+                              )                                  
+                            ],
+                          )
+                        ],
+                      ),
+
+                      Row
+                      (
+                        children: 
+                        [
+                          Icon
+                          (
+                            Icons.arrow_upward_outlined , 
+                            color: Colors.red,
+                            size: 45,
+                            
+                            
+                          ),
+                          SizedBox(width: 10,),
+                          Column
+                          (
+                            children: 
+                            [
+                              Text
+                              (
+                                "Income",
+                                style: TextStyle
+                                (
+                                  color: Colors.white,
+                                  fontSize: 18
+                                ),
+                          
+                              )    ,
+                              Text
+                              (
+                                "\$ 2000",
+                                style: TextStyle
+                                (
+                                  color: Colors.white,
+                                  fontSize: 20
+                                ),
+                          
+                              )                                  
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ]
+                
+              ),
+              
             )
           ],
         
